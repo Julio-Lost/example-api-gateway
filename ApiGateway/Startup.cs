fc3f37f4ac3772b2bda3ddf102c54751561edc37
juliocesarmedeiros <julio.medeiros@medsenior.com.br>
2020-12-08 11:11:05 -0300

    #jc instalado ocelot, instalado swagger nas apis e criado o arquivo de configuration.

---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(configuration);
+
+            services.AddSwaggerForOcelot(configuration);
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("API GATEWAY FUNCIONANDO");
+                });
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
