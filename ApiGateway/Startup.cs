3d24cb941cad059d4c8025f2bf067716669b927a
juliocesarmedeiros <julio.medeiros@medsenior.com.br>
2020-12-09 08:30:29 -0300

    #jc remoção de imports desnecessarios.

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ApiGateway
 {
