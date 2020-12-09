44ef2c2ccf57173694e06ab0bb24e5d234c289d9
juliocesarmedeiros <julio.medeiros@medsenior.com.br>
2020-12-09 08:22:43 -0300

    #jc adicionado o SwaggerForOcelot, Swagger Keys.

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -27,17 +27,28 @@ namespace ApiGateway
             services.AddOcelot(configuration);
 
             services.AddSwaggerForOcelot(configuration);
-            
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/gateway");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
+            app.UseSwaggerForOcelotUI(configuration, opt =>
+            {
+                opt.DownstreamSwaggerEndPointBasePath = "/gateway/swagger/docs";
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+
+            app.UseHttpsRedirection();
 
             app.UseHttpsRedirection();
 
